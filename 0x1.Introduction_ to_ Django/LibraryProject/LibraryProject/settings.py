08c7107f8f507925eafc10599383654d67c2f211
olaniSiyum <olisiyum@gmail.com>
2024-08-11 16:52:48 +0300

    Utilizing the Django Admin Interface

--- 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'alx_book_store',
+        'USER': 'root',
+        'PASSWORD': 'mysqloli3532',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
