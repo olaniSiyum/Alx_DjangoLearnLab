489443ec4edb264560c3acca94bcdc80c5c7c4e5
olani siyum <olisiyum@gmail.com>
2024-08-25 16:09:17 +0300

    Custom User Model in Django

---
+++ LibraryProject/bookshelf/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .managers import CustomUserManager  # Import the custom manager
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
