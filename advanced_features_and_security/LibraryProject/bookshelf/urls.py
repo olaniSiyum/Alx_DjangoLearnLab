b5229c969dc7a0803a0e8933ce262d0dee88c66a
olani siyum <olisiyum@gmail.com>
2024-08-25 23:09:55 +0300

    file added and view.py modified

---
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import example_view
+
+urlpatterns = [
+    path('example/', example_view, name='example_view'),
+    # Add other URL patterns as needed
+]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect
 from .models import CustomUser
 from .models import Book
+from django.shortcuts import render
+from .forms import ExampleForm
+
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -28,3 +31,19 @@ def book_list(request):
     books = Book.objects.all()  # Fetch all books from the database
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data, e.g., create or update a model instance
+            title = form.cleaned_data['title']
+            author = form.cleaned_data['author']
+            publication_year = form.cleaned_data['publication_year']
+            # Example: Create a new Book instance (if applicable)
+            # Book.objects.create(title=title, author=author, publication_year=publication_year)
+            # Redirect or render a success page
+            return redirect('success_url')  # Update with your success URL
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
