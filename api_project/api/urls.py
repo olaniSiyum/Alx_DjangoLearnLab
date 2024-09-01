d64f2d6dfc1403009bb1ffedd3d03946bf1ff9fa
olani siyum <olisiyum@gmail.com>
2024-09-01 13:50:16 +0300

    CRUD Operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
