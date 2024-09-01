b1b951b2971b4d39916e2b9c617a9201652c84c9
olani siyum <olisiyum@gmail.com>
2024-09-01 14:01:26 +0300

    Authentication and Permissions in Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,7 @@ from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
@@ -10,4 +11,5 @@ router.register(r'books', BookViewSet, basename='book')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
