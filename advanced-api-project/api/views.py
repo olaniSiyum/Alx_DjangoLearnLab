7e6e6f261a058b8fd106837041c8b0e8e08f5fd9
olani siyum <olisiyum@gmail.com>
2024-09-08 13:14:22 +0300

    modified view.py file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,8 +5,7 @@
 # BookDeleteView: Allows authenticated users to delete a book from the database.
 
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.exceptions import ValidationError
