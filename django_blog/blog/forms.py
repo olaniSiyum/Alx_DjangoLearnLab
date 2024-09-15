45449a3c121ec79482ea792b0e052a5038442891
olani siyum <olisiyum@gmail.com>
2024-09-15 23:07:50 +0300

    modified form and model

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Tag
 from .models import Comment
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -33,6 +34,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),  # Use TagWidget for tags
+        }
+
 
 
 class CommentForm(forms.ModelForm):
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .models import Post
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField('Tag', related_name='posts', blank=True)
+    tags = TaggableManager()  # Use TaggableManager for tags
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
