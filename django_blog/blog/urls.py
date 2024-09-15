1b0313ff3322c533e6d3a8848b6824863741387b
olani siyum <olisiyum@gmail.com>
2024-09-15 23:25:39 +0300

    modified url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -39,4 +39,7 @@ urlpatterns = [
     path('search/', search_posts, name='search-posts'),
     path('tags/<str:tag_name>/', PostListView.as_view(), name='posts-by-tag'),
     path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts_by_tag'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
